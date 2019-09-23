f25955b47f266a24acefb33db856c0d01d6b9588
Phumthawan <knzphumthawan@gmail.com>
2019-09-23 23:10:23 +0700

    useEffect by id

--- src/components/useEffect/DataFetching.js
+++ src/components/useEffect/DataFetching.js
@@ -3,28 +3,36 @@ import axios from 'axios'
 
 function DataFetching() {
 
-    const [posts, setPosts] = useState([])
+    // const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState({})
+    const [id, setId] = useState(1)
 
     useEffect(() => {
-        axios.get('http://jsonplaceholder.typicode.com/posts')
+        axios.get(`http://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res => {
                 console.log(res.data)
                 setPosts(res.data)
             })
             .catch(err => console.log(err))
 
-        return () => {
+    }, [id])
 
-        };
-    }, [])
+    // const postList = posts.map(post => (
+    //     <li key={post.id}>{post.title}</li>
+    // ))
 
-    const postList = posts.map(post => (
-        <li key={post.id}>{post.title}</li>
-    ))
+    const { title } = posts
 
     return (
         <div>
-            <ul>{postList}</ul>
+            <input
+                type="text"
+                value={id}
+                onChange={e => setId(e.target.value)}
+            />
+            <br />
+            {title}
+            {/* <ul>{postList}</ul> */}
         </div>
     )
 }
