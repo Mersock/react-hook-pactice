9f9f116a9e97d49b1d4cade9a65f1b279fec23a7
Phumthawan <knzphumthawan@gmail.com>
2019-09-24 22:35:26 +0700

    initial useContext

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MouseContainer from './components/useEffect/MouseContainer';
 import IntervalCalssCounter from './components/useEffect/IntervalCalssCounter';
 import IntervalHookCounter from './components/useEffect/IntervalHookCounter';
 import DataFetching from './components/useEffect/DataFetching';
+import ComponentC from './components/useContext/ComponentC';
 
 
 function App() {
@@ -32,7 +33,8 @@ function App() {
       {/* <MouseContainer/> */}
       {/* <IntervalCalssCounter/> */}
       {/* <IntervalHookCounter/> */}
-      <DataFetching/>
+      {/* <DataFetching/> */}
+      <ComponentC />
     </div>
   );
 }
