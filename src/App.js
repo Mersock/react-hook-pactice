9a95741f6ee4db7c4e195cb24446f9041ea1b949
Phumthawan <knzphumthawan@gmail.com>
2019-09-26 22:37:50 +0700

    use state with use context

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
-import ClassCounter from './components/useState/ClassCounter';
-import HookCounter from './components/useState/HookCounter';
-import HookCounterTwo from './components/useState/HookCounterTwo';
-import HookCounterThree from './components/useState/HookCounterThree';
-import ClassCounterTwo from './components/useState/ClassCounterTwo';
-import HookCounterFour from './components/useState/HookCounterFour';
-import ClassCounterOne from './components/useEffect/ClassCounterOne';
-import HookCounterOne from './components/useEffect/HookCounterOne';
-import ClassMouse from './components/useEffect/ClassMouse';
-import HookMouse from './components/useEffect/HookMouse';
-import MouseContainer from './components/useEffect/MouseContainer';
-import IntervalCalssCounter from './components/useEffect/IntervalCalssCounter';
-import IntervalHookCounter from './components/useEffect/IntervalHookCounter';
-import DataFetching from './components/useEffect/DataFetching';
-import ComponentC from './components/useContext/ComponentC';
-import CounterOne from './components/useReducer/CounterOne';
-import CounterTwo from './components/useReducer/CounterTwo';
+// import ClassCounter from './components/useState/ClassCounter';
+// import HookCounter from './components/useState/HookCounter';
+// import HookCounterTwo from './components/useState/HookCounterTwo';
+// import HookCounterThree from './components/useState/HookCounterThree';
+// import ClassCounterTwo from './components/useState/ClassCounterTwo';
+// import HookCounterFour from './components/useState/HookCounterFour';
+// import ClassCounterOne from './components/useEffect/ClassCounterOne';
+// import HookCounterOne from './components/useEffect/HookCounterOne';
+// import ClassMouse from './components/useEffect/ClassMouse';
+// import HookMouse from './components/useEffect/HookMouse';
+// import MouseContainer from './components/useEffect/MouseContainer';
+// import IntervalCalssCounter from './components/useEffect/IntervalCalssCounter';
+// import IntervalHookCounter from './components/useEffect/IntervalHookCounter';
+// import DataFetching from './components/useEffect/DataFetching';
+// import ComponentC from './components/useContext/ComponentC';
+// import CounterOne from './components/useReducer/CounterOne';
+// import CounterTwo from './components/useReducer/CounterTwo';
+// import CounterThree from './components/useReducer/CounterThree';
+import ComponentA from './components/useReducerWithuseContext/ComponentA'
+import ComponentB from './components/useReducerWithuseContext/ComponentB'
+import ComponentC from './components/useReducerWithuseContext/ComponentC'
 
-export const UserContext = React.createContext()
-export const ChannelContext = React.createContext()
+export const CountContext = React.createContext()
+
+// export const UserContext = React.createContext()
+// export const ChannelContext = React.createContext()
+
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className="App">
       {/* <ClassCounter />
@@ -46,8 +69,16 @@ function App() {
       </UserContext.Provider> */}
 
       {/* <CounterOne/> */}
-
-      <CounterTwo/>
+      {/* <CounterTwo/> */}
+      {/* <CounterThree/> */}
+      <CountContext.Provider
+        value={{ countState: count, countDispatch: dispatch }}
+      >
+        Count - {count}
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </CountContext.Provider>
     </div>
   );
 }
